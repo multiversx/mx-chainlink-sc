0b408d69f92dcc71e6988730fbc1d73b5ff1c25f
mateigraura <grauramatei@gmail.com>
2021-07-12 14:35:22 +0300

    fix nonce overlap external calls

--- elrond-adapter/interaction/interactor.go
+++ elrond-adapter/interaction/interactor.go
@@ -1,13 +1,14 @@
 package interaction
 
 import (
+	"net/http"
 	"sync"
 
 	"github.com/ElrondNetwork/elrond-adapter/config"
 	logger "github.com/ElrondNetwork/elrond-go-logger"
-	"github.com/ElrondNetwork/elrond-sdk/erdgo"
-	"github.com/ElrondNetwork/elrond-sdk/erdgo/blockchain"
-	"github.com/ElrondNetwork/elrond-sdk/erdgo/data"
+	"github.com/ElrondNetwork/elrond-sdk-erdgo"
+	"github.com/ElrondNetwork/elrond-sdk-erdgo/blockchain"
+	"github.com/ElrondNetwork/elrond-sdk-erdgo/data"
 )
 
 var log = logger.GetOrCreate("interaction")
@@ -19,6 +20,7 @@ type BlockchainInteractor struct {
 	gasPrice   uint64
 	privateKey []byte
 	publicKey  string
+	proxy      blockchain.ProxyHandler
 	account    *data.Account
 	txMut      sync.Mutex
 }
@@ -29,12 +31,13 @@ func NewBlockchainInteractor(chainInfo config.BlockchainInformation) (*Blockchai
 		return nil, err
 	}
 
+	proxy := blockchain.NewElrondProxy(chainInfo.ProxyUrl, &http.Client{})
+
 	addressHandler, err := data.NewAddressFromBech32String(pk)
 	if err != nil {
 		return nil, err
 	}
 
-	proxy := blockchain.NewElrondProxy(chainInfo.ProxyUrl)
 	account, err := proxy.GetAccount(addressHandler)
 	if err != nil {
 		return nil, err
@@ -47,23 +50,19 @@ func NewBlockchainInteractor(chainInfo config.BlockchainInformation) (*Blockchai
 		gasPrice:   chainInfo.GasPrice,
 		privateKey: sk,
 		publicKey:  pk,
+		proxy:      proxy,
 		account:    account,
 	}, nil
 }
 
 func (bi *BlockchainInteractor) SendTx(tx *data.Transaction) (string, error) {
-	bi.txMut.Lock()
-	defer bi.txMut.Unlock()
-
-	proxy := blockchain.NewElrondProxy(bi.proxyUrl)
-	txHash, err := proxy.SendTransaction(tx)
+	txHash, err := bi.proxy.SendTransaction(tx)
 	if err != nil {
 		log.Debug("failed sending transaction", "err", err.Error())
 		return "", err
 	}
 
-	log.Info("current account nonce", "nonce", bi.account.Nonce)
-	bi.account.Nonce++
+	log.Info("current account nonce", "nonce", tx.Nonce)
 	return txHash, nil
 }
 
@@ -72,6 +71,19 @@ func (bi *BlockchainInteractor) CreateSignedTx(
 	inputData []byte,
 	receiver string,
 ) (*data.Transaction, error) {
+	bi.txMut.Lock()
+	defer bi.txMut.Unlock()
+
+	addressHandler, err := data.NewAddressFromBech32String(bi.publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	bi.account, err = bi.proxy.GetAccount(addressHandler)
+	if err != nil {
+		return nil, err
+	}
+
 	tx := &data.Transaction{
 		Value:    value,
 		RcvAddr:  receiver,
@@ -85,7 +97,7 @@ func (bi *BlockchainInteractor) CreateSignedTx(
 		Options:  0,
 	}
 
-	err := erdgo.SignTransaction(tx, bi.privateKey)
+	err = erdgo.SignTransaction(tx, bi.privateKey)
 	if err != nil {
 		log.Debug("failed signing transaction", "err", err.Error())
 		return nil, err
